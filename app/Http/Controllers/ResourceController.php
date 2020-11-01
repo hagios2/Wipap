6df3f156b326c559d8496e14a43eaac8cedb50ab
Emmanuel Oteng Wilson <ewilson@npontu.com>
2020-11-01 16:37:55 +0000

    added route for garbage types

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -11,18 +11,6 @@ use App\Http\Resources\CampusResource;
 
 class ResourceController extends Controller
 {
-
-    public function getCampus()
-    {
-        return CampusResource::collection(Campus::all('id', 'campus'));
-    }
-
-
-    public function getShopTypes()
-    {
-        return CampusResource::collection(ShopType::all('id', 'shop_type'));
-    }
-
     public function getGarbageTypes()
     {
        return GarbageType::all('id', 'garbage_type');
