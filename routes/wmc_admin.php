e8a70ef500108c3abee80ac58b5b0b58919f4f1b
Emmanuel Oteng Wilson <ewilson@npontu.com>
2020-11-01 10:42:38 +0000

    changes

--- routes/wmc_admin.php
+++ routes/wmc_admin.php
@@ -3,7 +3,7 @@
 
 #============================ WMC Admin Routes =============================================
 
-Route::namespace('WMCController')->group(function () {
+Route::namespace('WMCControllers')->group(function () {
 
     Route::group(['prefix' => 'auth'], function () {
 
