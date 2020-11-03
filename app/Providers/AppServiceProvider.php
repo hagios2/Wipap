40b415d24516b032badfe911a98993df6d49da4b
Emmanuel Oteng Wilson <ewilson@npontu.com>
2020-11-03 01:24:51 +0000

    removed resource wrapping

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Http\Resources\Json\Resource;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 
@@ -32,5 +33,6 @@ class AppServiceProvider extends ServiceProvider
     {
        /*  $this->registerPolicies();
  */
+        Resource::withoutWrapping();
     }
 }
